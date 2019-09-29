91bbdaeea86d836164bdd377bdb3afe820b3d313
Henry Zimmerman <zimhen7@gmail.com>
2019-09-28 22:04:00 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+use yew::{Properties, Html, Component, ComponentLink, ShouldRender, Renderable};
+
+
+pub trait PureComponent: Properties + Emissive + PartialEq + Sized + 'static {
+    fn render(&self) -> Html<Pure<Self>>;
+}
+
+pub trait Emissive {
+    type Message;
+    fn emit(&self, msg: Self::Message);
+}
+
+#[derive(Debug)]
+pub struct Pure<T>(T);
+
+
+impl <T: PureComponent + Emissive + PartialEq + 'static> Component for Pure<T> {
+    type Message = <T as Emissive>::Message;
+    type Properties = T;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Pure(props)
+    }
+
+    fn update(&mut self, msg: Self::Message) -> bool {
+        self.0.emit(msg);
+        false
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        if self.0 != props {
+            self.0 = props;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl <T: PureComponent + Emissive + PartialEq + 'static> Renderable<Pure<T>> for Pure<T> {
+    fn view(&self) -> Html<Pure<T>> {
+        self.0.render()
+    }
+}
+
