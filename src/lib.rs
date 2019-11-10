438abe18f705b56a04897314f28cef4d00ea5f82
Henry Zimmerman <zimhen7@gmail.com>
2019-11-10 10:37:14 -0500

    add with_callback helper

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ pub mod dsl;
 mod not_equal_assign;
 mod pure;
 
+mod with_callback;
+
 pub mod ptr;
 
 mod history;
@@ -11,5 +13,6 @@ mod history;
 pub use history::History;
 pub use not_equal_assign::NeqAssign;
 pub use pure::{Emissive, Pure, PureComponent, PureEmissiveComponent};
+pub use with_callback::WithCallback;
 
 pub use yewtil_macro::Emissive;
