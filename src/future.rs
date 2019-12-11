3e5a2a223b472807eac5937352f98bf20c142da6
Hugues Morisset <morisset.hugues@gmail.com>
2019-12-11 13:48:31 +0100

    Stdweb spawn local future (#38)
    * Fix not equal assign example
    
    * Rename `send_self` `send_self_batch` to `send_message` `send_message_batch`
    
    * Use stdweb::spawn_local to execute futures sent to send_future
    
    * Add AgentLinkFuture trait and implem
    
    Allow to execute futures from Agents aswell as from Components
    
    * Only one LinkFuture trait for both ComponentLink and AgentLink

--- src/future.rs
+++ src/future.rs
@@ -1,10 +1,10 @@
 use std::future::Future;
-use yew::{ComponentLink, Component};
-use wasm_bindgen::JsValue;
-use wasm_bindgen_futures::future_to_promise;
+use yew::{ComponentLink, Component, agent::{AgentLink, Agent}};
+use stdweb::spawn_local;
 
-/// Trait that allows you to use `ComponentLink`s to register futures.
-pub trait ComponentLinkFuture {
+
+/// Trait that allows you to use `ComponentLink` and `AgentLink` to register futures.
+pub trait LinkFuture {
     type Message;
     /// This method processes a Future that returns a message and sends it back to the component's
     /// loop.
@@ -19,7 +19,7 @@ pub trait ComponentLinkFuture {
     fn send_future_batch<F>(&self, future: F) where F: Future<Output=Vec<Self::Message>> + 'static;
 }
 
-impl <COMP: Component> ComponentLinkFuture for ComponentLink<COMP> {
+impl <COMP: Component> LinkFuture for ComponentLink<COMP> {
     type Message = COMP::Message;
 
     fn send_future<F>(&self, future: F) where F: Future<Output=Self::Message> + 'static {
@@ -27,9 +27,8 @@ impl <COMP: Component> ComponentLinkFuture for ComponentLink<COMP> {
         let js_future = async move{
             let message: COMP::Message = future.await;
             link.send_message(message);
-            Ok(JsValue::NULL)
         };
-        future_to_promise(js_future);
+        spawn_local(js_future);
 
     }
 
@@ -38,8 +37,25 @@ impl <COMP: Component> ComponentLinkFuture for ComponentLink<COMP> {
         let js_future = async move {
             let messages: Vec<COMP::Message> = future.await;
             link.send_message_batch(messages);
-            Ok(JsValue::NULL)
         };
-        future_to_promise(js_future);
+        spawn_local(js_future);
+    }
+}
+
+impl <AGN: Agent> LinkFuture for AgentLink<AGN> {
+    type Message = AGN::Message;
+
+    fn send_future<F>(&self, future: F) where F: Future<Output=Self::Message> + 'static {
+        let link: AgentLink<AGN>  = self.clone();
+        let js_future = async move{
+            let message: AGN::Message = future.await;
+            let cb = link.callback(|m: AGN::Message| m);
+            cb.emit(message);
+        };
+        spawn_local(js_future);
+    }
+
+    fn send_future_batch<F>(&self, _future: F) where F: Future<Output=Vec<Self::Message>> + 'static {
+        unimplemented!("Agents don't support batching their messages.")
     }
 }
